8722522cfd9016b808221369068b094bc2138d33
Alexander Berg <72527002+johnalexanderberg@users.noreply.github.com>
2021-09-11 11:41:15 +0200

    add event listener for touchscreen move

--- javascript/utils.js
+++ javascript/utils.js
@@ -2,7 +2,7 @@ const pi = Math.PI;
 
 const container = document.querySelector('body');
 let distance = 1;
-const easing = (num) => Math.pow(num, 1.5);
+const easing = (num) => Math.pow(num, 2);
 
 //returns y point of wave
 function sineWave(x, amplitude, period, time){
@@ -12,7 +12,7 @@ const windowWidthFactor = window.innerWidth/1000 + 1;
 
 // outputs distance from center, range 0-1
 function calculateDistance([x, y]) {
-    const center = [window.innerWidth, 0];
+    const center = [innerWidth / 2, window.innerHeight / 2];
     const maxHypot = Math.hypot(center[0], center[1]);
     const hypot = Math.hypot(center[0] - x, center[1] - y);
 
@@ -26,6 +26,7 @@ const handleMove = ({ clientX, clientY }) => {
 };
 
 container.addEventListener("mousemove", handleMove);
+container.addEventListener("touchmove", handleMove);
 
 
 
